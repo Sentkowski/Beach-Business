792211ba518687e01da90b5b70711c4217a994a2
Szymon Sentkowski <szysen123@gmail.com>
2019-03-30 23:40:38 +0100

    Updates

---
+++ precache-manifest.b95e760190e707ffdfe1fa4394ac4765.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9e6089bf91ecee55abbcc032fcee273",
+    "url": "/UI-Beach/static/media/shop-image-white.e9e6089b.svg"
+  },
+  {
+    "revision": "d764fd6063cdf0490363e59dd5225c15",
+    "url": "/UI-Beach/static/media/shop-image-red.d764fd60.svg"
+  },
+  {
+    "revision": "4888b5c2a5a8bc20b4df6f75857c7c41",
+    "url": "/UI-Beach/static/media/shop-image-blue.4888b5c2.svg"
+  },
+  {
+    "revision": "45e8695a3f04dc169c1a759f811166b5",
+    "url": "/UI-Beach/static/media/beach.45e8695a.svg"
+  },
+  {
+    "revision": "361d4be068f4dee5a7ed",
+    "url": "/UI-Beach/static/js/runtime~main.5265e7dd.js"
+  },
+  {
+    "revision": "fe6bed1e9d00280cffd4",
+    "url": "/UI-Beach/static/js/main.cd7786fd.chunk.js"
+  },
+  {
+    "revision": "6f0318e6ae55755b4b2c",
+    "url": "/UI-Beach/static/js/2.2aa3cefc.chunk.js"
+  },
+  {
+    "revision": "fe6bed1e9d00280cffd4",
+    "url": "/UI-Beach/static/css/main.2ed2693a.chunk.css"
+  },
+  {
+    "revision": "8e0039c9d45b9fab584e1ca8a2c48e3e",
+    "url": "/UI-Beach/index.html"
+  }
+];
